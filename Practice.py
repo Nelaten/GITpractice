3aa87d2fd5183c106fd17c8b097d097608ed7c36
Nelson Muia <nelsonmuia777@gmail.com>
2025-03-23 13:17:51 +0300

    Added another line of code statement about Malili

--- Practice.py
+++ Practice.py
@@ -1,3 +1,4 @@
 print("Hello world")
 print("Nelson and Seshat are eternal")
 print("From the world of the Supreme Being, all can be learnt, including Data science")
+print("Extra line of a ka code")
